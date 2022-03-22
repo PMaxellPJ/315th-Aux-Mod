1f26523cdbf2657a3c9cf20e84746aef7dcf42c4
PMaxellPJ <Pjmaxx924@gmail.com>
2022-03-22 15:45:21 -0400

    Removed unneccesary files. File name changes. Edited config.cpp to allow for proper reading of mod

--- addons/gear/config.cpp
+++ addons/gear/config.cpp
@@ -1,12 +1,10 @@
-#include "script_component.hpp"
-
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {QGVAR(ammoBox), QGVAR(medicalBox), QGVAR(launcherBox), QGVAR(alicepack), QGVAR(IbuprofenItem), QGVAR(Zeus_Pack), QGVAR(Zeus_Pack_Invisible), QGVAR(kitbag_lr), QGVAR(falconii_lr), QGVAR(eagleiii_lr), QGVAR(carryall_lr), "tfar_inviisble_radio"};
+        units[] =
         weapons[] = {QGVAR(Pilot_Radio_Pack)}
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {QUOTE(MAIN_ADDON), "A3_Data_F", "A3_Weapons_F", "ace_medical_engine", "ace_medical_treatment"};
+        requiredAddons[] =
         author = "AUTHOR";
         VERSION_CONFIG;
     };
@@ -16,4 +14,4 @@ class CfgInventoryGlobalVariable {
     maxSoldierLoad = 2000;
 };
 
-#include "CfgGear.hpp"
+#include "CfgWeapons.hpp"

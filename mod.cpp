e36dbfe65ecd34a12c5dd4c52ff513abeba94651
PMaxellPJ <Pjmaxx924@gmail.com>
2022-03-22 10:27:00 -0400

    no message

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
- name             = "315th_Aux"; // Name of your mod
- author         = "author_mod"; // Affects Arma 3 Launcher, when the mod are loaded as local
- action         = "hhttps://github.com/PMaxellPJ/315th-Aux-Mod"; // Website URL, that can be accessed from the expansions menu
- overview         = "Aux mod for the 315th IID."; // Supports structured text
- hideName         = 1; // Hide the extension name
- hidePicture    = 1;    // Hide the extension menu
+ name             = "315th_Aux";
+ author         = "author_mod";
+ action         = "hhttps://github.com/PMaxellPJ/315th-Aux-Mod";
+ overview         = "Aux mod for the 315th IID.";
+ hideName         = 1;
+ hidePicture    = 1;

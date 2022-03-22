f114a9d3e2ff6c0474ab433ecc1436424a538806
PMaxellPJ <75550119+PMaxellPJ@users.noreply.github.com>
2022-03-22 10:14:27 -0400

    Add files via upload

---
+++ addons/gear/CfgGear.hpp
@@ -0,0 +1,15 @@
+		//Imports
+		class SWLB_clone_bag_belt;
+		
+		class GVAR(Pilot_Radio_Pack):SWKB_clone_bag_belt {
+				DISPLAYNAME(Pilot Radio Pack)
+		model = QPATHTOF(data\radio.p3d);
+		mass = 0;
+		tf_dialogUpdate = QUOTE(call TFAR_fnc_updateLRDialogToChannel;);
+		tf_hasLRradio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "tf_west_radio_code";
+		tf_subtype = "digital_lr";
+		tf_range = 120000;
+		maximumLoad = 500;
+		

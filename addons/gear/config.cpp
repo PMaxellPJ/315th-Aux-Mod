938fbc85eb4aa7438bb5c47c9fac7e2f071f71b6
PMaxellPJ <Pjmaxx924@gmail.com>
2022-03-23 18:26:52 -0400

    Line 8        requiredAddons[] = {QUOTE(MAIN_ADDON), "A3_Data_F", "A3_Weapons_F", "ace_medical_engine", "ace_medical_treatment"};

--- addons/gear/config.cpp
+++ addons/gear/config.cpp
@@ -3,8 +3,9 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-       units[] = {QGVAR(Pilot_Radio_Pack)};
-        requiredVersion =;
+        units[] = {QGVAR(Pilot_Radio_Pack)};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {QUOTE(MAIN_ADDON), "A3_Data_F", "A3_Weapons_F", "ace_medical_engine", "ace_medical_treatment"};
         author = "AUTHOR";
         VERSION_CONFIG =;
     };

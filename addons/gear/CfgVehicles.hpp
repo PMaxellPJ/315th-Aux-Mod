9169cf21c089dde0d56acf1d1ea574e9ea1fd244
PMaxellPJ <Pjmaxx924@gmail.com>
2022-03-23 19:33:54 -0400

    IS is basically RHS big sad

--- addons/gear/CfgVehicles.hpp
+++ addons/gear/CfgVehicles.hpp
@@ -1,15 +1,13 @@
 class CfgVehicles {    
-    //Imports
-    class SWLB_clone_bag_belt;
-    class GVAR(Pilot_Radio_Pack) : SWLB_clone_bag_belt {
-    DISPLAYNAME(Pilot Radio Pack)
-    mass = 0;
-    tf_dialogUpdate = QUOTE(call TFAR_fnc_updateLRDialogToChannel;);
-    tf_hasLRradio = 1;
-    tf_encryptionCode = "tf_west_radio_code";
-    tf_dialog = "is_empireST_backpack_radio";
-    tf_subtype = "digital_lr";
-    tf_range = 120000;
-    maximumLoad = 500;
-    };
+        class SWLB_clone_bag_belt;
+        class GVAR(Pilot_Radio_Pack):SWLB_clone_bag_belt {
+        DISPLAYNAME(Pilot Radio Pack)
+        tf_dialogUpdate = QUOTE(call TFAR_fnc_updateLRDialogToChannel;);
+        tf_hasLRradio = 1;
+        tf_encryptionCode = "tf_west_radio_code";
+        tf_dialog = "rt1523g_radio_dialog";
+        tf_subtype = "digital_lr";
+        tf_range = 120000;
+        maximumLoad = 500;
+        };
 };
